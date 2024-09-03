59cd449a434602a5d37b3d3af16d993d4f5f189a
Jhoel <cjhoel199@gmail.com>
2024-09-03 17:36:09 -0400

    Comit 2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -10,9 +10,5 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-=======
-abstract class Controller
-{
-    //
->>>>>>> b74367831dd8814575f63701300f1a811d3e6e91
+
 }
